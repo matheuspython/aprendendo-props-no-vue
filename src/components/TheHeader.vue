218cd3edb3beae162ff14b19757b5e0a7b0b8874
Matheus Fonseca de Souza <matheus.fonseca@contabilizei.com.br>
2022-08-09 17:39:36 -0300

    app

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    {{title}}
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'TheHeader',
+    props: {
+        title: String
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
